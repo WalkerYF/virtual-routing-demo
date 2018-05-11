1282fa6bc53c1b2280977b28e6e8c0f48b3bf5f0
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-05-11 13:22:29 +0800

    fix bugs

--- src/route.py
+++ src/route.py
@@ -14,14 +14,14 @@ class Interface():
 class Route():
     def __init__(self, config_file):
         self.name = 'test'
-        self.route_table = {
-            
-        }
+        self.route_table = {}
         # TODO:读取配置文件，将接口状态写入
         # 此时应该建立连接，获取用于模拟物理连接的socket
         # self.interface
     def route_table_init(self):
         # TODO:使用接口，初始化路由表
+        raise NotImplementedError()
     
-    def data_link_layer(ip_package, next_hop_ip):
+    def data_link_layer(self, ip_package, next_hop_ip):
         # TODO:将一个ip包扔到这个函数，然后就会根据最长匹配原则，选择一个端口发送。
+        raise NotImplementedError()
