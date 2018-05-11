72ce928e90c68dcbf342a1d9974e2e3efcd748e4
walker <WalkerYF@yeah.net>
2018-05-11 10:57:34 +0800

    add data_link_layer function

--- src/route.py
+++ src/route.py
@@ -1,14 +1,27 @@
 import socket
+import json
+
+
 
 class Interface():
-    def __init__(self, ip, net_mask, next_hop):
-        self.ip = ip
+    def __init__(self, src_ip, dest_ip, net_mask, link_simulation):
+        self.src_ip = src_ip
+        self.dest_ip = dest_ip
         self.net_mask = net_mask
-        self.next_hop = next_hop
+        self.link_simulation = socket
 
 
 class Route():
     def __init__(self, config_file):
         self.name = 'test'
+        self.route_table = {
+            
+        }
         # TODO:读取配置文件，将接口状态写入
+        # 此时应该建立连接，获取用于模拟物理连接的socket
         # self.interface
+    def route_table_init(self):
+        # TODO:使用接口，初始化路由表
+    
+    def data_link_layer(ip_package):
+        # TODO:将一个ip包扔到这个函数，然后就会根据最长匹配原则，选择一个端口发送。
