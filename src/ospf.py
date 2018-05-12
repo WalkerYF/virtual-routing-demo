0663a2f5742f7ff4e9a0f1e00f58d3af01286276
walker <WalkerYF@yeah.net>
2018-05-12 22:07:35 +0800

    修复了若干bug 增加了路由无法到达的提示信息

--- src/ospf.py
+++ src/ospf.py
@@ -1,7 +1,45 @@
 import route
+import json
+import sys
+from include.utilities import IP_Package
 
+config_name = sys.argv[1]
+with open(config_name, 'r') as config_f:
+    config = json.load(config_f)
 
+# 初始化网络层
+network_layer = route.NetworkLayer(config)
 
+# 添加路由表项
+# route.my_route_table.update_item('8.8.4.0', 24, '8.8.1.3')
 
+while True:
+    s = input()
+    print(s)
+    if s == 'show interface':
+        route.link_layer.show_interface()
+    elif s == 'show tcp':
+        route.link_layer.show_tcp()
+    elif s == 'show route table':
+        route.my_route_table.show()
+    elif s == 'add':
+        dest_net = input('dest_net : ')
+        net_mask = input('net_mask : ')
+        next_ip = input('next_ip : ')
+        route.my_route_table.update_item(dest_net, net_mask, next_ip)
+    elif s == 'send':
+        src_ip = input('src_ip : ')
+        final_ip = input('final_ip :')
+        data = input('data : ')
+        network_layer.send(src_ip,final_ip,data.encode('ascii'))
+    elif s == 'test add':
+        route.my_route_table.update_item('8.8.4.0',24,'8.8.1.3')
+    elif s == 'test send':
+        network_layer.send('8.8.1.2', '8.8.4.2', b'testtest!!!!')
+    else:
+        continue        
 
-if __name__ == '__main__':
+# while True:
+#     network_layer.recv()
+
+# if __name__ == '__main__':
--- src/route.py
+++ src/route.py
@@ -31,6 +31,7 @@ Interface = link.Host
 link_layer = link.DataLinkLayer()
 my_route_table = RouteTable()
 
+# 注意! 下面的两个队列存放对象，而不是二进制数据
 # 用来存储网络层需要向上传递的ip包
 route_recv_package = queue.Queue(0)
 # 用来存储网络层需要发送的ip包
@@ -51,8 +52,7 @@ class TransmitThread(threading.Thread):
             while route_send_package.qsize() == 0:
                 continue
             # 从队列中获得一个包
-            send_package = route_send_package.get()
-            ip_package = IP_Package.bytes_package_to_objdect(send_package)
+            ip_package = route_send_package.get()
 
             # DEBUG信息
             logger.debug(' this package will be modiflied according to route table !')
@@ -60,7 +60,9 @@ class TransmitThread(threading.Thread):
 
             # 使用成员函数处理IP包，修改其中的dest_ip字段，获得新的IP包
             ret_ip_package = self.ip_package_handler(ip_package)
-
+            if ret_ip_package == None:
+                logger.debug('{} is unreachable. \nShow your route table by "show route table"'.format(ip_package.final_ip))
+                continue
             # DEBUG信息
             logger.debug(' had modifly !')
             logger.debug(ret_ip_package)
@@ -73,10 +75,13 @@ class TransmitThread(threading.Thread):
         使用转发表对IP包进行处理
         1. 使用目的ip获得其下一跳ip:dest_ip
         2. 使用其目的ip，获取目的子网掩码
+        如果路由表找不到，返回None
         """
         final_ip = ip_pkg.final_ip
         # 从转发表中获得去往该子网需要的下一跳路由和路由所在子网
         dest_ip_net_mask = my_route_table.get_dest_ip(final_ip)
+        if dest_ip_net_mask == None:
+            return None
         # 修改ip包的下一跳路由
         ip_pkg.dest_ip = dest_ip_net_mask[0]
         ip_pkg.net_mask = dest_ip_net_mask[1]
@@ -96,10 +101,10 @@ class MonitorLinkLayer(threading.Thread):
             is_local = my_route_table.is_local_link(ip_package.final_ip)
             if is_local:
                 # 网络层要了IP包，并交给了更上层的协议
-                route_recv_package.put(recv_data)
+                route_recv_package.put(ip_package)
             else:
-                # 网络层修改了ip包，要转发
-                route_send_package.put(recv_data)
+                # 网络层修改ip包，要转发
+                route_send_package.put(ip_package)
 
 my_transmit_thread = TransmitThread()
 my_monitor_link_layer = MonitorLinkLayer()
@@ -158,18 +163,20 @@ class NetworkLayer():
             interfaces.append(new_interface)
         link_layer.host_register(interfaces)
 
-    def send(self, ip_package_data : bytes, src_ip : str,  final_ip : str):
+    def send(self, src_ip : str,  final_ip : str, ip_package_data : bytes):
         """ 只需要将这个包放到队列中即可，另一个线程负责队列中的包处理并发送出去 """
+        # TODO:疑问：网络层的接口，是否需要提供源ip？
+        # 我应该到了链路层才知道使用哪一个接口发送呀，才知道接口对应的IP呀！
         # 应用层应该只需要知道目的IP就好了，然后源ip在网络层中获取，子网掩码和目的ip由转发表设置
         # 这里ip包的子网掩码是目的网络的子网掩码，应用层是不知道的
-        ip_pkb = IP_Package(src_ip, final_ip, final_ip, 0, ip_package_data)
-        route_send_package.put(ip_package)
+        ip_pkg = IP_Package(src_ip, final_ip, final_ip, 0, ip_package_data)
+        route_send_package.put(ip_pkg)
 
     def recv(self) -> bytes :
         """ 阻塞式接受IP包，该IP包一定是发给自己，需要处理的 """
         while route_recv_package.qsize == 0:
             continue
-        route_recv_package.get()
+        return route_recv_package.get()
 
     def update_route_table(self):
         pass
@@ -192,7 +199,7 @@ if __name__ == "__main__":
         s = input("Route {} >".format(route.name))
         print(s)
         if s == 'show ipv4 interface':
-            link_layer.show()
+            link_layer.show_interface()
         elif s == 'show tcp':
             link_layer.show_tcp()
         else:
