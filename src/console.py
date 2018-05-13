9aeb5da5c33b00e6887be51aa70081c3f12fd456
walker <WalkerYF@yeah.net>
2018-05-13 15:28:36 +0800

    在 console.py中增加了一部分注释

--- src/console.py
+++ src/console.py
@@ -1,25 +1,35 @@
-import route
 import os
 import json
 import sys
-from include.utilities import IP_Package
 import threading
 import logging
 import time
-import re
+
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.contrib.completers import WordCompleter
 
-logging.basicConfig(
-    # filename='../../log/client.{}.log'.format(__name__),
-    format='[%(asctime)s - %(name)s - %(levelname)s] : \n%(message)s\n',
-    # datefmt='%M:%S',
-)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+import route
+from include.utilities import IP_Package
+# logging.basicConfig(
+#     # filename='../../log/client.{}.log'.format(__name__),
+#     format='[%(asctime)s - %(name)s - %(levelname)s] : \n%(message)s\n',
+#     # datefmt='%M:%S',
+# )
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
 
+# 初始化网络层(内部还会初始化链路层)
+config_name = sys.argv[1]
+with open(config_name, 'r') as config_f:
+    config = json.load(config_f)
+network_layer = route.NetworkLayer(config)
+
+
+
+# NOTICE:如果需要使用终端，将以下代码复制到对应脚本的if __name__ == '__main__':下，并且import上面的库
+# NOTICE:我是默认你们用终端之前已经import route并且初始化了网络层的，所以下面的代码直接使用网络层的接口
 Completer = WordCompleter(['show help', 'show tcp', 'show interface', 'show route', 'add', 'send', 'recv'],
                              ignore_case=True)
 help_menu = [
@@ -32,13 +42,6 @@ help_menu = [
     'delete dest_net net_mask\n\t: delete an item in route table \n\texample : delete 8.8.3.0 24"',
     'recv\n\t: no arguments',
 ]
-
-# 初始化网络层(内部还会初始化链路层)
-config_name = sys.argv[1]
-with open(config_name, 'r') as config_f:
-    config = json.load(config_f)
-network_layer = route.NetworkLayer(config)
-
 while True:
     user_input = prompt('Route {}>'.format(network_layer.name), 
                         history=FileHistory('history.txt'),
@@ -46,9 +49,10 @@ while True:
                         completer=Completer,
                         )
     try:
+        if user_input == '':
+            continue
         # 拆分用户参数
         user_args = user_input.split()
-        # logger.debug(user_args)
         main_action = user_args[0]
 
         # 解析参数
@@ -65,10 +69,13 @@ while True:
                     print('-'*40)
                     print(help_msg)
         elif main_action == 'add':
+            # 往路由表中增加表项
             route.my_route_table.update_item(user_args[1], int(user_args[2]), user_args[3])
         elif main_action == 'delete':
+            # 删除某一项
             route.my_route_table.delete_item(user_args[1], int(user_args[2]))
         elif main_action == 'send':
+            # 发送信息
             network_layer.send(user_args[1], user_args[2], user_args[3].encode('ascii'))
         elif main_action == 'recv':
             # 非阻塞接受IP包
@@ -83,7 +90,7 @@ while True:
         print('invalid command!')
         continue
     except Exception as e:
-        # print ('e.message:\t{}'.format(e.message))
+        # 捕获所有异常，并且打印错误信息
         print(traceback.format_exc())
         continue
 
