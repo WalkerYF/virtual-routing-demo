eec10df0e8f46cd4a01a81f33ce7bd283ab94047
walker <WalkerYF@yeah.net>
2018-05-16 01:45:05 +0800

    统一logger的设置，修改了rip模块中的logger位置。

--- src/include/logger.py
+++ src/include/logger.py
@@ -1,4 +1,7 @@
 import logging
+
+logging.addLevelName( logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName( logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
 logging.basicConfig(
     # filename='../../log/client.{}.log'.format(__name__),
     format='[%(asctime)s - %(name)s - %(levelname)s] : \n%(message)s\n',
