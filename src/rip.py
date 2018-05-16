fc6b777febde8997d5dd3dd27b6c49cecc49816c
李新锐 <hongwen000@outlook.com>
2018-05-16 11:25:07 +0800

    添加个人报告，删除rip中一些多余的代码

--- src/rip.py
+++ src/rip.py
@@ -4,10 +4,8 @@ import route
 import threading
 import logging
 import json
-import datetime
 import time
 import sys
-import hashlib
 import include.utilities as utilities
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
@@ -63,13 +61,9 @@ class RIP(threading.Thread):
             time.sleep(2)
 
     def broadcast(self, interfaces):
-        md5 = hashlib.md5()
-        md5.update((self.route_name + str(time.time())).encode('utf-8'))
         rip_msg = {
             "from" : self.route_name,
-            # "intfs": [(inf.vip, inf.netmask) for inf in interfaces],
             "topo": self.topo,
-            "md5" : md5.hexdigest(),
             'tear_down': self.tear_down,
             "dv": self.dis_vec
         }
@@ -81,7 +75,7 @@ class RIP(threading.Thread):
 
     def process(self, rip_msg : dict):
         medium = rip_msg['from']
-        # 阻止含有之前已经处理过的离线的路由器的信息的rip报文
+        # 步骤一：阻止含有之前已经处理过的离线的路由器的信息的rip报文
         if medium in self.tear_down:
             return
         
@@ -93,14 +87,14 @@ class RIP(threading.Thread):
 
         for rname in rip_msg['topo'].keys():
             if rname in self.tear_down:
-                logger.debug("[RIP] [Dropped] TOPO of %s contain offline route %s", medium, drname)
+                logger.debug("[RIP] [Dropped] TOPO of %s contain offline route %s", medium, rname)
                 return
 
         logger.debug("[RIP] received rip package from %s", medium)
         new_tear_down = set(rip_msg['tear_down']) - set(self.tear_down)
         self.tear_down = list(set().union(self.tear_down, rip_msg['tear_down']))
 
-        # 删除自身所有有关已经离线了的路由器的信息
+        # 步骤二：删除自身所有有关新得知的离线了的路由器的信息
         # 删除距离向量中的这一行
         if len(new_tear_down) != 0:
             to_del = []
@@ -144,21 +138,16 @@ class RIP(threading.Thread):
                     for vip, netmask in intfs:
                         logger.info("[RIP] Updating route table\n{} {} THROUTH {}".format(vip, netmask, self.next_hop[dest]))
                         route.my_route_table.update_item(vip, netmask, self.next_hop[dest])
-        
+
+        # 步骤三：更新自己的距离向量和拓扑图,因为可能学习到新的节点
         # if medium not in self.topo.keys():
         #     self.topo[medium] = rip_msg['intfs']
-        if rip_msg['md5'] in self.received_set:
-            logger.info("[RIP] droped, already received")
-            pass
-        elif medium not in self.dis_vec.keys():
+        if medium not in self.dis_vec.keys():
                 logger.critical("Unexpected rip msg from %s", rip_msg['from'])
-                self.received_set.add(rip_msg['md5'])
         else:
-            msg_used = True
             mininum = DV_INF
             for dest, detail in rip_msg['dv'].items():
                 if not dest in self.topo.keys():
-                    msg_used = False
                     continue
                 cost = detail['cost']
                 newcost = self.dis_vec[medium]['cost'] + cost
@@ -178,23 +167,29 @@ class RIP(threading.Thread):
                         logger.info("[RIP] Updating route table\n{} {} THROUTH {}".format(vip, netmask, self.next_hop[medium]))
                         route.my_route_table.update_item(vip, netmask, self.next_hop[medium])
 
+        # 步骤四：更新距离矩阵中的一行
         self.dis_mat[medium] = rip_msg['dv']
         logger.debug('{}'.format(utilities.obj_to_beautiful_json(self.dis_mat)))
-        # print(self.dis_mat)
+        # 步骤五：运行核心的dv算法
         for dest, detail in self.dis_vec.items():
+            # 直连的链路cost不在这里更新cost，只在cost change protocol中更新
             if dest in self.direct_routes:
                 continue
+            # 不需要对未知子网Interfaces情况的路由器计算距离，计算了也没用，也加不了路由表
             if not dest in self.topo.keys():
                 continue
             mininum = DV_INF
             new_path = []
             medium = ''
-            bool_changed = False
             for entry_route, dv in self.dis_mat.items():
+                # 去不到就continue，相当于设置了正无穷
                 if dest not in dv.keys():
                     continue
+
                 new_cost = self.dis_vec[entry_route]['cost'] + dv[dest]['cost']
                 if new_cost < mininum:
+                    # 毒性逆转(poisoned reverse)
+                    # 如果可能的最短路径中包含目的节点自身
                     if dest in dv[dest]['path'] and dest != dv[dest]['path'][-1]:
                         continue
                     logger.debug('maybe shortest : {} to {}'.format(self.route_name, dest))
@@ -203,9 +198,9 @@ class RIP(threading.Thread):
                     medium = entry_route
                     new_path = dv[dest]['path']
                     if new_path[0] != self.route_name:
-                        bool_changed = True
                         new_path.insert(0, self.route_name)
 
+            # 更新距离向量和路由表，并打印适量调试信息
             old_cost = detail['cost']
             detail['cost'] = mininum
             if mininum >= DV_INF:
@@ -220,23 +215,6 @@ class RIP(threading.Thread):
             if old_cost != detail['cost']:
                  logger.info("[RIP] New shortest path found\n{} -> {} cost {}, path: {}".format(
                      self.route_name, dest, self.dis_vec[dest]['cost'], self.dis_vec[dest]['path']))
-
-
-
-                    # # print(dest, newcost, self.dis_vec[dest]['cost'])
-                    # if newcost < self.dis_vec[dest]['cost']:
-                    #     self.dis_vec[dest]['cost'] = newcost
-                    #     newpath = detail['path']
-                    #     # logger.warning('New Path is {}, now append {} to it'.format(newpath, medium))
-                    #     newpath.insert(0, medium)
-                    #     self.dis_vec[dest]['path'] = newpath
-                    #     logger.info("[RIP] New shortest path found\n{} -> {} cost {}, path: {}".format(
-                    #         self.route_name, dest, self.dis_vec[dest]['cost'], self.dis_vec[dest]['path']))
-                    #     for vip, netmask in self.topo[dest]:
-                    #         logger.info("[RIP] Updating route table\n{} {} THROUTH {}".format(vip, netmask, self.next_hop[medium]))
-                    #         route.my_route_table.update_item(vip, netmask, self.next_hop[medium])
-            if msg_used:
-                self.received_set.add(rip_msg['md5'])
 
     def show_dv(self):
         x = PrettyTable(['Dest', 'Cost', 'Path'])
@@ -406,38 +384,3 @@ if __name__ == "__main__":
             # 捕获所有异常，并且打印错误信息
             print(traceback.format_exc())
             continue
-            # 解析参数
-            # if main_action == 'show':
-            #     if user_args[1] == 'interface':
-            #         route.link_layer.show_interface()
-            #     elif user_args[1] == 'tcp':
-            #         route.link_layer.show_tcp()
-            #     elif user_args[1] == 'route':
-            #         route.my_route_table.show()
-            #     elif user_args[1] == 'help':
-            #         print('This is help message!')
-            #         for help_msg in help_menu:
-            #             print('-'*40)
-            #             print(help_msg)
-            #     elif user_args[1] == 'dv':
-            #         rip_worker.show_dv()
-            #     elif user_args[1] == 'off':
-            #         print(rip_worker.tear_down)
-            # elif main_action == 'add':
-            #     # 往路由表中增加表项
-            #     route.my_route_table.update_item(user_args[1], int(user_args[2]), user_args[3])
-            # elif main_action == 'delete':
-            #     # 删除某一项
-            #     route.my_route_table.delete_item(user_args[1], int(user_args[2]))
-            #     for rname, lvip_mask in rip_worker.topo.items():
-            #         for lvip, lmask in lvip_mask:
-            #             if user_args[1] == lvip and int(user_args[2]) == lmask:
-            #                 if rname in rip_worker.direct_routes:
-            #                     print("Cannot delete, {} is a directly connected route")
-            #                 else:
-            #                     logger.info('[RIP] Reset cost %s to %s to INF', network_layer.name, rname)
-            #                     rip_worker.dis_vec[rname]['cost'] = DV_INF
-            #                     rip_worker.dis_vec[rname]['path'] = []
-
- 
-
